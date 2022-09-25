77f9313077e89ea05710ddd85f9721de3e099f47
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-25 18:01:29 +0400

    debugging deploy

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ export network=goerli
 cat deploy.yaml | \
 sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-sed 's/${starknet_archive_image}/starknet_archive_image/g' | sed "s@starknet_archive_image@${image}@g" |\
+sed 's/${image_archive}/image_archive/g' | sed "s@image_archive@${image_archive}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
 sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
 kubectl --namespace $namespace -f - apply
@@ -21,7 +21,7 @@ export network=mainnet
 cat deploy.yaml | \
 sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-sed 's/${starknet_archive_image}/starknet_archive_image/g' | sed "s@starknet_archive_image@${image}@g" |\
+sed 's/${image_archive}/image_archive/g' | sed "s@image_archive@${image_archive}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
 sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
 kubectl --namespace $namespace -f - apply
