a9b1843962b93ce492dff4f0bc78e2c09991d9ff
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-25 15:54:30 +0400

    use db schema determined by starknet network selection

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 env | grep 'namespace\|network\|db_host\|image_archive'
 
-envsubst < deploy.yaml | kubectl --namespace $namespace -f - apply --dry-run
+network=goerli envsubst < deploy.yaml | kubectl --namespace $namespace -f - apply
 
+network=mainnet envsubst < deploy.yaml | kubectl --namespace $namespace -f - apply
