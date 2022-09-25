494f8d84432871aee7d7504cd45b3db8a38af190
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-25 17:36:13 +0400

    debugging deploy

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
 sed 's/${starknet_archive_image}/starknet_archive_image/g' | sed "s@starknet_archive_image@${image}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
-sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g"
+sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
 kubectl --namespace $namespace -f - apply
 
 export network=mainnet
