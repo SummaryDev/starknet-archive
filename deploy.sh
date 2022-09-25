e849ce75b224aa64608dc3c7a5f4b3476e653583
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-25 17:04:21 +0400

    replaced image_archive with image

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-env | grep 'namespace\|network\|db_host\|image_archive'
+env | grep 'namespace\|network\|db_host\|image'
 
 #network=goerli envsubst < deploy.yaml | kubectl --namespace $namespace -f - apply
 #
@@ -11,7 +11,7 @@ export network=goerli
 cat deploy.yaml | \
 sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-sed 's/${image_archive}/image_archive/g' | sed "s@image_archive@${image_archive}@g" |\
+sed 's/${image}/image/g' | sed "s@image@${image}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
 sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
 kubectl --namespace $namespace -f - apply
@@ -21,7 +21,7 @@ export network=mainnet
 cat deploy.yaml | \
 sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-sed 's/${image_archive}/image_archive/g' | sed "s@image_archive@${image_archive}@g" |\
+sed 's/${image}/image/g' | sed "s@image@${image}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
 sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
 kubectl --namespace $namespace -f - apply
