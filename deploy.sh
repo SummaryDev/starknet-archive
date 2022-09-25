05f2f0c255942134ed1a27737b645ddc338e83e0
Oleg Abdrashitov <olegabu73@gmail.com>
2022-09-25 17:28:00 +0400

    debugging deploy

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,17 @@ export network=goerli
 cat deploy.yaml | \
 sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
 sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-sed 's/${image}/image/g' | sed "s@image@${image}@g" |\
+sed 's/${starknet_archive_image}/starknet_archive_image/g' | sed "s@starknet_archive_image@${image}@g" |\
 sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
 sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g"
-#kubectl --namespace $namespace -f - apply
-#
-#export network=mainnet
-#
-#cat deploy.yaml | \
-#sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
-#sed 's/${network}/network/g' | sed "s/network/$network/g" | \
-#sed 's/${image}/image/g' | sed "s@image@${image}@g" |\
-#sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
-#sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
-#kubectl --namespace $namespace -f - apply
+kubectl --namespace $namespace -f - apply
+
+export network=mainnet
+
+cat deploy.yaml | \
+sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
+sed 's/${network}/network/g' | sed "s/network/$network/g" | \
+sed 's/${starknet_archive_image}/starknet_archive_image/g' | sed "s@starknet_archive_image@${image}@g" |\
+sed 's/${db_password_archive}/db_password_archive/g' | sed "s/db_password_archive/$db_password_archive/g" | \
+sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
+kubectl --namespace $namespace -f - apply
